bdf7770e20d6d95ec73c00d15bb4d69233d9d9de
Spolk4 <suzanne.m.polk@gmail.com>
2019-08-05 11:40:06 -0500

    review and edit of sytanx in app.py

--- StarterCode/Belly_Button_Biodiversity/app.py
+++ StarterCode/Belly_Button_Biodiversity/app.py
@@ -83,6 +83,7 @@ def samples(sample):
     stmt = db.session.query(Samples).statement
     df = pd.read_sql_query(stmt, db.session.bind)
 
+
     # Filter the data based on the sample number and
     # only keep rows with values above 1
     sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
